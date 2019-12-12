fd080907fb833ee547373f4731beb998bec6a2d5
Zax <ztalley@unifiedprincipal.com>
2019-12-12 10:06:19 -0600

    fixed pathway

--- server.js
+++ server.js
@@ -10,16 +10,16 @@ app.use(express.static("public"));
 app.use(express.static(__dirname + '/'))
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "./public/assets/index.html"));
 });
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "./public/assets/notes.html"));
 });
 
 app.get("/api/notes", function(req, res) {
 
-    let noteData = fs.readFileSync('./assets/db.json');
+    let noteData = fs.readFileSync('./public/assets/db.json');
     let savedNotes = JSON.parse(noteData);
     res.json(savedNotes);
 });
@@ -30,7 +30,7 @@ app.post("/api/notes", function(req, res) {
         title: req.body.title,
         text: req.body.text,
     };
-    let noteData = fs.readFileSync('./assets/db.json');
+    let noteData = fs.readFileSync('./public/assets/db.json');
     try {
         var convertedData = JSON.parse(noteData)
         res.json(true);
@@ -41,17 +41,17 @@ app.post("/api/notes", function(req, res) {
         currentNote.push(convertedData[i]);
     }
     currentNote.push(newNote);
-    fs.writeFile('./assets/db.json', JSON.stringify(currentNote), function(err) {
+    fs.writeFile('./public/assets/db.json', JSON.stringify(currentNote), function(err) {
         if (err) throw err;
     });
 });
 
 app.delete("/api/notes/:title", function(req, res) {
-    let noteData = fs.readFileSync('./assets/db.json');
+    let noteData = fs.readFileSync('./public/assets/db.json');
     try {
         var convertedData = JSON.parse(noteData)
         var chosenNote = convertedData.filter(note => note.title !== req.params.title)
-        fs.writeFile('./assets/db.json', JSON.stringify(chosenNote), function(err) {
+        fs.writeFile('./public/assets/db.json', JSON.stringify(chosenNote), function(err) {
             if (err) throw err;
         });
     } catch (err) {
